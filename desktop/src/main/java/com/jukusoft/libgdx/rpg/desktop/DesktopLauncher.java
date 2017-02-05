5a0b48226722a65dc73060345c359cf00ff58d5b
JuKu <kuenzel.justin@t-online.de>
2017-02-06 00:02:25 +0100

    added module.

--- desktop/src/main/java/com/jukusoft/libgdx/rpg/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/jukusoft/libgdx/rpg/desktop/DesktopLauncher.java
@@ -1,16 +1,18 @@
 package com.jukusoft.libgdx.rpg.desktop;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 /**
  * Created by Justin on 05.02.2017.
  */
 public class DesktopLauncher {
 
     public static void main (String[] args) {
-        /*LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Pentaquin";
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Test RPG Game";
         config.height = 640;
         config.width = 960;
-        config.addIcon("./data/assets/icon/Pentamoon-P.png", Files.FileType.Absolute);*/
+        //config.addIcon("./data/assets/icon/Pentamoon-P.png", Files.FileType.Absolute);
         //new LwjglApplication(new Game(), config);
     }
 

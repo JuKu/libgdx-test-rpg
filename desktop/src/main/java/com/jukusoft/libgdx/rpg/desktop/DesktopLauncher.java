eecbf10021ef07de8f390d30551356e9666f1dc9
JuKu <kuenzel.justin@t-online.de>
2017-02-05 23:50:19 +0100

    added main class.

---
+++ desktop/src/main/java/com/jukusoft/libgdx/rpg/desktop/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.jukusoft.libgdx.rpg.desktop;
+
+/**
+ * Created by Justin on 05.02.2017.
+ */
+public class DesktopLauncher {
+
+    public static void main (String[] args) {
+        /*LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Pentaquin";
+        config.height = 640;
+        config.width = 960;
+        config.addIcon("./data/assets/icon/Pentamoon-P.png", Files.FileType.Absolute);*/
+        //new LwjglApplication(new Game(), config);
+    }
+
+}

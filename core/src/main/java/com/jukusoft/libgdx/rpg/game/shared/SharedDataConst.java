06a890440886b44d363cb9a342cbf9cae041ef68
JuKu <kuenzel.justin@t-online.de>
2017-02-08 12:07:26 +0100

    added SharedDataConst.

---
+++ core/src/main/java/com/jukusoft/libgdx/rpg/game/shared/SharedDataConst.java
@@ -0,0 +1,10 @@
+package com.jukusoft.libgdx.rpg.game.shared;
+
+/**
+ * Created by Justin on 08.02.2017.
+ */
+public class SharedDataConst {
+
+    public static final String SAVE_NAME = "saveName";
+
+}

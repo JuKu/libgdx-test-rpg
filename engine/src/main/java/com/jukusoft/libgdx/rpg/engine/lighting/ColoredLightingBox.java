a772d0dcb3bbb0b1a6a0e88ccf15383fe3fdb599
JuKu <kuenzel.justin@t-online.de>
2017-02-10 12:07:05 +0100

    fixed some problems.

--- engine/src/main/java/com/jukusoft/libgdx/rpg/engine/lighting/ColoredLightingBox.java
+++ engine/src/main/java/com/jukusoft/libgdx/rpg/engine/lighting/ColoredLightingBox.java
@@ -9,10 +9,28 @@ import com.jukusoft.libgdx.rpg.engine.time.GameTime;
  */
 public class ColoredLightingBox {
 
+    protected float x = 0;
+    protected float y = 0;
+    protected float width = 0;
+    protected float height = 0;
+
     protected Color color = Color.BLACK;
+
+    public ColoredLightingBox (float x, float y, float width, float height, Color fillColor) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = fillColor;
+    }
+
+    public ColoredLightingBox (float x, float y, float width, float height) {
+        this(x, y, width, height, Color.BLACK);
+    }
 
     public void draw (GameTime time, ShapeRenderer shapeRenderer) {
         shapeRenderer.setColor(this.color);
+        shapeRenderer.rect(this.x, this.y, this.width, this.height);
     }
 
 }

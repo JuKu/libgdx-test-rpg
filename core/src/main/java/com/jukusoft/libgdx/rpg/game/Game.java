34a6c673ac12ba699b5f20e06e5b2254e0d0de3f
JuKu <kuenzel.justin@t-online.de>
2017-02-10 19:44:28 +0100

    changed Game, added basic input mappings for movements.

--- core/src/main/java/com/jukusoft/libgdx/rpg/game/Game.java
+++ core/src/main/java/com/jukusoft/libgdx/rpg/game/Game.java
@@ -1,10 +1,13 @@
 package com.jukusoft.libgdx.rpg.game;
 
+import com.badlogic.gdx.Input;
 import com.jukusoft.libgdx.rpg.engine.game.ScreenBasedGame;
+import com.jukusoft.libgdx.rpg.engine.input.InputMapper;
 import com.jukusoft.libgdx.rpg.engine.save.impl.DefaultSavedGameManager;
 import com.jukusoft.libgdx.rpg.engine.screen.IScreen;
 import com.jukusoft.libgdx.rpg.engine.screen.ScreenManager;
 import com.jukusoft.libgdx.rpg.engine.version.GameVersion;
+import com.jukusoft.libgdx.rpg.game.input.GameActionsConst;
 import com.jukusoft.libgdx.rpg.game.screen.*;
 import com.jukusoft.libgdx.rpg.game.utils.AssetPathUtils;
 
@@ -30,6 +33,20 @@ public class Game extends ScreenBasedGame {
 
         //set shader path
         this.shaderPath = AssetPathUtils.getShaderPath("");
+
+        //create input mapping
+        InputMapper inputMapper = getInputManager().getInputMapper();
+
+        //add mappings for movements
+        inputMapper.addMapping(Input.Keys.W, GameActionsConst.MOVE_UP);
+        inputMapper.addMapping(Input.Keys.S, GameActionsConst.MOVE_DOWN);
+        inputMapper.addMapping(Input.Keys.A, GameActionsConst.MOVE_LEFT);
+        inputMapper.addMapping(Input.Keys.D, GameActionsConst.MOVE_RIGHT);
+
+        inputMapper.addMapping(Input.Keys.UP, GameActionsConst.MOVE_UP);
+        inputMapper.addMapping(Input.Keys.DOWN, GameActionsConst.MOVE_DOWN);
+        inputMapper.addMapping(Input.Keys.LEFT, GameActionsConst.MOVE_LEFT);
+        inputMapper.addMapping(Input.Keys.RIGHT, GameActionsConst.MOVE_RIGHT);
 
         //create screen
         screenManager.addScreen("jukusoft_intro", new JuKuSoftIntroScreen());
